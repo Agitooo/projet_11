cd776639d4d0dc01c555ce91a259ec77c79aa213
Vincent <>
2023-06-29 04:14:18 +0200

    renommage fonctional_tests => unit_tests + ajout fixture pour test unitaire + retrait fixture, monkeypatch test integration + ajout login/logout test integration

--- tests/integration_tests/test_update_points.py
+++ tests/integration_tests/test_update_points.py
@@ -6,16 +6,17 @@ from GUDLFT.server import app
 class TestUpdatePoint:
     client = app.test_client()
 
-    def test_update_point(self, monkeypatch, competitions_fixture, clubs_fixture):
+    def test_update_point(self):
 
-        monkeypatch.setattr(server, "loadCompetitions", competitions_fixture)
-        monkeypatch.setattr(server, "loadClubs", clubs_fixture)
-
-        competitions = server.loadCompetitions
-        clubs = server.loadClubs
+        competitions = server.loadCompetitions()
+        clubs = server.loadClubs()
 
         competition = competitions[1]
         club = clubs[1]
+
+        # On se connecte
+        show_summary_result = self.client.post('/showSummary', data={"email": club["email"]})
+        assert f"Welcome, {club['email']}" in show_summary_result.data.decode()
 
         club_point = int(club['points'])
         place_to_book = 3
@@ -25,7 +26,7 @@ class TestUpdatePoint:
         assert f"<td>{club['name']}</td><td>{club_point}</td>".replace(" ", "") \
                in view_point_before.data.decode().replace(' ', '').replace("\n", "")
 
-        # On réserve 3 place et on contrôle que les 3 places sont bien soustraites
+        # On réserve 3 places et on contrôle que les 3 places sont bien soustraites
         result = self.client.post(
             '/purchasePlaces',
             data={
@@ -41,3 +42,6 @@ class TestUpdatePoint:
         assert f"<td>{club['name']}</td><td>{(club_point - place_to_book)}</td>".replace(" ", "") \
                in view_point_after.data.decode().replace(' ', '').replace("\n", "")
 
+        # On se déconnecte
+        logout_result = self.client.get('/logout')
+        assert logout_result.status_code == 302
--- tests/unit_tests/test_purchase.py
+++ tests/unit_tests/test_purchase.py
@@ -1,14 +1,31 @@
+import GUDLFT.server as server
 from GUDLFT.server import app
 
 
 class TestPurchase:
     client = app.test_client()
 
-    def test_purchase_place_invalid_max_12(self):
-        competition = "Spring Festival"
-        club = "Simply Lift"
-        places = "13"
-        result = self.client.post('/purchasePlaces', data={"competition": competition, "club": club, "places": places})
+    def test_purchase_place_invalid_max_12(self, monkeypatch, competitions_fixture, clubs_fixture):
+
+        monkeypatch.setattr(server, "loadCompetitions", competitions_fixture)
+        monkeypatch.setattr(server, "loadClubs", clubs_fixture)
+
+        competitions = server.loadCompetitions
+        clubs = server.loadClubs
+
+        competition = competitions[0]
+        club = clubs[0]
+        places_to_purchase = "13"
+
+        result = self.client.post(
+            '/purchasePlaces',
+            data={
+                "competition": competition['name'],
+                "club": club['name'],
+                "places": places_to_purchase
+            }
+        )
+
         assert result.status_code == 200
         assert "You can only buy a maximum of 12 places" in result.data.decode()
 
